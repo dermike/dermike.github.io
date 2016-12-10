7e5225768b064a5db6db95c45275cc0f35a64329
Mikael Jergefelt <mike@redvolume.com>
2016-12-10 13:42:57 +0100

    fix linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-var gulp = require('gulp');
-var concatCss = require('gulp-concat-css'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    minifycss = require('gulp-minify-css');
+const gulp = require('gulp');
+const concatCss = require('gulp-concat-css');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const minifycss = require('gulp-minify-css');
 
-gulp.task('css', function() {
+gulp.task('css', () => {
   gulp.src(['css/cssreset.css', 'css/grid.css', 'css/style.css', 'css/responsive_custom.css', 'css/print.css'])
-    .pipe(concatCss("bundle.css"))
+    .pipe(concatCss('bundle.css'))
     .pipe(minifycss())
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
   gulp.src(['node_modules/smooth-scroll/dist/js/smooth-scroll.js', 'node_modules/gumshoe/dist/js/gumshoe.js', 'js/custom.js'])
     .pipe(uglify())
     .pipe(concat('all.js'))
